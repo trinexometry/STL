00c4b84ff949f87e22f840f02c5043dcd580eeda
gaurav sharam <trinexometry@gmail.com>
2023-03-12 17:24:00 +0530

    vectors of vectors

--- nestedvect.cpp
+++ nestedvect.cpp
@@ -46,7 +46,33 @@ int main()
     {
         printVec(v1[i]);
     }
+
+
+    //vector of vector
+    int M;
+    cout<<"ENTER THE SIZE OF VECTOR: ";
+    cin>>M;
+    vector<vector<int>> vec;
     
+    for(int i = 0; i<M; i++){
+        int n;
+        cout<<"how many values: ";
+        cin>>n;
+        vector<int> temp;
+        for (int j = 0; j < n; j++)
+        {
+            int x;
+            cout<<"enter the element : ";
+            cin>>x;
+            temp.push_back(x);
+        }
+        vec.push_back(temp);
+        
+    }
+
+    for(int i = 0; i <3; i++){
+        printVec(vec[i]);
+    }
 
     return 0;
 }
