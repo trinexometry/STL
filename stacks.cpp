40855a49352cf18581ec1153cb91d99bb19143d5
gaurav sharam <trinexometry@gmail.com>
2023-03-18 15:28:33 +0530

    stacks

---
+++ stacks.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//LAST IN FIRST OUT DATA STRUCTURE
+
+void print_Stack(stack<int> &s){
+    while (s.empty() == false)
+    {
+        cout<<" ' "<<s.top()<<" ' "<<endl;
+        s.pop();
+    }
+    
+}
+
+int main()
+{   stack<int> s;
+    s.push(10);
+    s.push(134);
+    s.push(1);
+
+    cout<<"size: "<<s.size();
+    cout<<endl;
+
+    print_Stack(s);
+    
+
+
+    return 0;
+}
